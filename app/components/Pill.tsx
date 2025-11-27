6354a2042b24e73c921437a50c276880dffaf68a
Zulkifli Firdausi <65410126+Zulkifli70@users.noreply.github.com>
2025-11-27 13:17:19 +0700

    add category page (routing)

--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -1,8 +1,9 @@
 import { PillProps } from "@/app/types";
 
-export default function Pill({ children, className = "" }: PillProps) {
+export default function Pill({ children, className = "", ...rest }: PillProps) {
   return (
     <span
+      {...rest}
       className={`inline-block bg-transparent border border-gray-400 rounded-full px-3 py-1 text-sm text-gray-800 ${className}`}
     >
       {children}
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import type { ReactNode } from "react";
+import type { ReactNode, HTMLAttributes } from "react";
 
 export type Model = {
   id: number;
@@ -10,10 +10,25 @@ export type Model = {
   dateAdded: string;
 };
 
+export type Category = {
+  displayName: string;
+  slug: string;
+};
+
+export type CategoriesData = {
+  categories: Category[];
+};
+
 // Page Types
 export type RootLayoutProps = Readonly<{
   children: React.ReactNode;
 }>;
+
+export type CategoryPageProps = {
+  params: Promise<{
+    categoryName: string;
+  }>;
+};
 
 export type ModelDetailPageProps = {
   params: Promise<{
@@ -26,7 +41,12 @@ export type ModelCardProps = {
   model: Model;
 };
 
+export type ModelsGridProps = {
+  title: string;
+  models: Model[];
+};
+
 export type PillProps = {
   children: ReactNode;
   className?: string;
-};
+} & HTMLAttributes<HTMLSpanElement>;
